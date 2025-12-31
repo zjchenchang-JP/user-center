4ccf7498ee3e5525288ad2b76db312897896c84d
zjchenchang <zjchenchang@gmail.com>
2025-12-31 23:23:05 +0900

    注册功能/register function

--- zjcc-user-center-backend/src/main/java/com/zjcc/usercenter/mapper/UserMapper.java
+++ zjcc-user-center-backend/src/main/java/com/zjcc/usercenter/mapper/UserMapper.java
@@ -1,8 +1,18 @@
 package com.zjcc.usercenter.mapper;
 
+import com.zjcc.usercenter.model.domain.User;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.zjcc.usercenter.model.User;
 
+/**
+* @author 86187
+* @description 针对表【user(用户表)】的数据库操作Mapper
+* @createDate 2025-12-31 18:59:01
+* @Entity generator.domain.User
+*/
 public interface UserMapper extends BaseMapper<User> {
 
 }
+
+
+
+
---
+++ zjcc-user-center-backend/src/main/java/com/zjcc/usercenter/utils/StaticConst.java
@@ -0,0 +1,6 @@
+package com.zjcc.usercenter.utils;
+
+// MD5加密的盐值
+public interface StaticConst {
+    public static final String SALT = "z1j9c9c2";
+}
