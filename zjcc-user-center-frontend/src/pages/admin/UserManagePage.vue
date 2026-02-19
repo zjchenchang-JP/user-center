3be6c4c3b7d3384e6894db78fcdcc2a7db330f42
zjchenchang <zjchenchang@gmail.com>
2026-02-19 23:12:00 +0900

    feat: 完善用户管理页面 增加搜索功能

--- zjcc-user-center-frontend/src/pages/admin/UserManagePage.vue
+++ zjcc-user-center-frontend/src/pages/admin/UserManagePage.vue
@@ -1,57 +1,61 @@
 <template>
+  <div>
+    <a-input-search
+      style="max-width: 300px; margin-bottom: 20px; margin-left: 73%;"
+      v-model:value="searchValue"
+      placeholder="搜索用户名"
+      enter-button="Search"
+      size="large"
+      @search="onSearch"
+    />
   <a-table :columns="columns" :data-source="data">
-    <template #headerCell="{ column }">
-      <template v-if="column.key === 'name'">
-        <span>
-          <smile-outlined />
-          Name
-        </span>
-      </template>
-    </template>
-
     <template #bodyCell="{ column, record }">
-      <template v-if="column.key === 'name'">
-        <a>
-          {{ record.name }}
-        </a>
+      <template v-if="column.dataIndex === 'avatarUrl'">
+        <a-image :src="record.avatarUrl" :width="100" />
       </template>
-      <template v-else-if="column.key === 'tags'">
-        <span>
-          <a-tag
-            v-for="tag in record.tags"
-            :key="tag"
-            :color="
-              tag === 'loser'
-                ? 'volcano'
-                : tag.length > 5
-                ? 'geekblue'
-                : 'green'
-            "
-          >
-            {{ tag.toUpperCase() }}
-          </a-tag>
-        </span>
+      <template v-else-if="column.dataIndex === 'userRole'">
+        <div v-if="record.userRole === 1">
+          <a-tag color="green">管理员</a-tag>
+        </div>
+        <div v-else>
+          <a-tag color="blue">普通用户</a-tag>
+        </div>
+      </template>
+      <template v-else-if="column.dataIndex === 'createTime'">
+        {{ dayjs(record.createTime).format("YYYY-MM-DD HH:mm:ss") }}
       </template>
       <template v-else-if="column.key === 'action'">
-        <span>
-          <a>Invite 一 {{ record.name }}</a>
-          <a-divider type="vertical" />
-          <a>Delete</a>
-          <a-divider type="vertical" />
-          <a class="ant-dropdown-link">
-            More actions
-            <down-outlined />
-          </a>
-        </span>
+        <a-button danger @click="doDelete(record.id)">删除</a-button>
       </template>
     </template>
   </a-table>
+  </div>
 </template>
 <script lang="ts" setup>
-import { searchUsers } from "@/api/user";
+import { deleteUser, searchUsers } from "@/api/user";
 import { SmileOutlined, DownOutlined } from "@ant-design/icons-vue";
 import { message } from "ant-design-vue";
-import { ref } from "vue";
+import { reactive, ref } from "vue";
+import dayjs from "dayjs";
+const searchValue = ref();
+// 搜索按钮触发
+const onSearch = () =>{
+  fetchData(searchValue.value);
+}
+
+// 删除
+const doDelete = async (id:string) =>{
+  if(!id){
+    return
+  }
+  const res = await deleteUser(id);
+  if (res.data.code === 0) {
+    message.success("删除成功")
+  } else {
+    message.error("删除失败")
+  }
+}
+
 const columns = [
   {
     title: "id",
@@ -91,14 +95,13 @@ const columns = [
 const data = ref([]);
 
 // 获取数据
-const fetchData = async () =>{
-  const res = await searchUsers("");
-  if (res.data.data){
-    data.value = res.data.data
+const fetchData = async (username = "") => {
+  const res = await searchUsers(username);
+  if (res.data.data) {
+    data.value = res.data.data;
   } else {
-    message.error("获取数据失败")
+    message.error("获取数据失败");
   }
-}
+};
 fetchData();
-
 </script>
